5e8064d371630f9f74be947e69e9bd03f684efad
Hayoung <gkduddy0814@gmail.com>
2023-05-01 00:23:30 +0900

    graphql 제거 및 only prisma and server components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
-import { Suspense } from "react";
+import { Suspense } from 'react';
+import { getStocks } from 'frontend/prisma/queries';
+import Loading from './loading';
+
+export const revalidate = 3600; // revalidate every hour
 
 export default async function HomePage() {
+  const stocks = await getStocks();
   return (
     <>
-      <Suspense>
+      <Suspense fallback={<Loading />}>
         HomePage
+        {stocks.map((a) => {
+          return <div key={a.stockCode}>{a.stockName}</div>;
+        })}
       </Suspense>
     </>
   );
---
+++ frontend/prisma/queries/stock.ts
@@ -0,0 +1,10 @@
+import prisma from 'backend/prisma/prisma';
+
+export function sleep(ms: number) {
+  return new Promise((r) => setTimeout(r, ms));
+}
+export async function getStocks() {
+  await sleep(3000);
+  const posts = await prisma.stock.findMany();
+  return posts;
+}
