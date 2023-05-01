fc9f55f2ade5f26069708992ea39e51675c43e36
Hayoung <gkduddy0814@gmail.com>
2023-05-01 16:51:51 +0900

    lodaing ui 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react';
+import Link from 'next/link';
 import { getStocks } from 'frontend/prisma/queries';
-import Loading from './loading';
 
 export const revalidate = 3600; // revalidate every hour
 
@@ -8,11 +8,12 @@ export default async function HomePage() {
   const stocks = await getStocks();
   return (
     <>
-      <Suspense fallback={<Loading />}>
-        HomePage
+      <Suspense>
+        주식종목 리스트
         {stocks.map((a) => {
           return <div key={a.stockCode}>{a.stockName}</div>;
         })}
+        <Link href={'/create'}>등록하기</Link>
       </Suspense>
     </>
   );
