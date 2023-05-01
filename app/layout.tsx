b9a048bdef1f4ba829c681055d38b38907e00984
Hayoung <gkduddy0814@gmail.com>
2023-05-01 16:51:31 +0900

    feat : next/font 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode, Suspense } from 'react';
-import { Inter } from 'next/font/google';
+import { Noto_Sans_KR } from 'next/font/google';
 import clsx from 'clsx';
 
 import Navbar from 'frontend/components/Layout/navbar';
@@ -10,29 +10,37 @@ const { SITE_NAME } = process.env;
 
 export const metadata = {
   title: {
-    default: SITE_NAME,
+    default: `${SITE_NAME}`,
     template: `%s | ${SITE_NAME}`
   },
   robots: {
     follow: true,
     index: true
-  }
+  },
+  description: `${SITE_NAME} Description`,
+  keywords: ['Keyword']
 };
 
-const inter = Inter({
+const noto_kr = Noto_Sans_KR({
   subsets: ['latin'],
+  weight: ['400', '500', '700', '900'],
   display: 'swap',
-  variable: '--font-inter'
+  style: ['normal'],
+  variable: '--font-noto'
 });
 
 export default async function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={inter.variable}>
-      <body className={clsx(`bg-dark text-white selection:bg-fuchsia-600 selection:text-white`)}>
+    <html lang="en" className={clsx(noto_kr.className)}>
+      <body
+        className={clsx(
+          `m-auto max-w-xl bg-gray-200 text-white selection:bg-fuchsia-600 selection:text-white`
+        )}
+      >
         {/* @ts-expect-error Server Component */}
         <Navbar />
         <Suspense>
-          <main className="px-5 pt-6">{children}</main>
+          <main className="min-h-screen bg-dark px-5 pt-6">{children}</main>
         </Suspense>
       </body>
     </html>
