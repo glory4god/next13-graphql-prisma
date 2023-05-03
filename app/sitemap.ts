96a5574d0b9ccf7ec0627d99b6c7041520108f13
Hayoung <gkduddy0814@gmail.com>
2023-05-03 21:42:08 +0900

    fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,7 +6,7 @@ const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
   : 'http://localhost:3000';
 
 export default async function sitemap(): Promise<Promise<Promise<MetadataRoute.Sitemap>>> {
-  const routesMap = ['', '/search'].map((route) => ({
+  const routesMap = ['', '/create'].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString()
   }));
